26cb1849d51b982d72e6bf269f9f7eb744757f8e
Kojun123 <kwonojou@gmail.com>
2025-05-05 18:23:51 +0900

    security 설정

---
+++ src/main/java/com/example/eightmonthcheckpoint/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+
+package com.example.eightmonthcheckpoint.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        System.out.println("시큐리티 설정 로딩됨");
+
+        http
+                .formLogin(form -> form
+                        .loginPage("/customLogin").permitAll()
+                        .loginProcessingUrl("/doLogin")
+                        .defaultSuccessUrl("/loginSuccess", true)
+                        .permitAll()
+                )
+                .authorizeHttpRequests(auth -> auth
+                                .requestMatchers("/", "/customLogin", "/doLogin", "/css/**", "/js/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                .requestMatchers("/css/**", "/js/**", "/images/**", "/WEB-INF/**");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
