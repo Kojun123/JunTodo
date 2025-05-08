25e6b52d31e209c534dc1b35e44bd450257c8fc6
Kojun123 <kwonojou@gmail.com>
2025-05-08 22:02:05 +0900

    오류수정 entity graph 적용

--- src/main/java/com/example/eightmonthcheckpoint/repository/TodoRepository.java
+++ src/main/java/com/example/eightmonthcheckpoint/repository/TodoRepository.java
@@ -1,11 +1,13 @@
 package com.example.eightmonthcheckpoint.repository;
 
 import com.example.eightmonthcheckpoint.domain.Todo;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TodoRepository extends JpaRepository<Todo, Long> {
@@ -15,4 +17,7 @@ public interface TodoRepository extends JpaRepository<Todo, Long> {
 
     @Query("SELECT t From Todo t WHERE upper(t.completed) = 'ON'  ORDER BY CASE t.priority WHEN 'HIGH' THEN 1 WHEN 'MEDIUM' THEN 2 WHEN 'LOW' THEN 3 END, t.completed, t.id, t.createdAt ")
     List<Todo> findByCompletedOn();
+
+    @EntityGraph(attributePaths = {"user"})
+    Optional<Todo> findById(Long id);
 }
